30a8d97fdf97e1d00a46d9c87a9b74346386d9e2
Lukas Grossar <lukas.grossar@adfinis-sygroup.ch>
2020-02-21 12:48:53 +0100

    Extend test to check for invalid characters

--- test_lines.py
+++ test_lines.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 
 import sys
+import string
 import textwrap
 
 DATE_PLACEHOLDER = "XXX DD.MM"
+ALLOWED_CHARACTERS = set(string.ascii_letters + string.digits + " " + "." + "/" + "-")
 
 fails = False
 with open(sys.argv[1], "r") as fh:
     for i, line in enumerate(fh.read().splitlines()):
+        # Check for invalid characters
+        if not set(line).issubset(ALLOWED_CHARACTERS):
+            fails = True
+            print("Line %i contains invalid characters: %s" % (i + 1, line))
+
+        # Check for wordwrapping
         text = "%s %s" % (DATE_PLACEHOLDER, line)
         lines = textwrap.wrap(text, 26)
         if len(lines) > 2:
