a2691665380ea5e03be461fecb4dbea70d485f79
Lukas Grossar <lukas.grossar@adfinis.com>
2022-10-05 16:00:21 +0200

    chore(flappytime.py): paint It, Black

--- flappytime.py
+++ flappytime.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 FLAPPY_DATA = "https://raw.githubusercontent.com/adfinis/flappy-sez/main/flappy.txt"
 
-MSG_CACHE_FILE = '/tmp/flappytime.txt'
+MSG_CACHE_FILE = "/tmp/flappytime.txt"
 
 HELP_TEXT = """
 Get a random message and put it on display.
@@ -32,35 +32,35 @@ Here's some more options you can use:
 
 
 def new_message():
-    lines = requests.get(FLAPPY_DATA).content.decode('utf-8').splitlines()
+    lines = requests.get(FLAPPY_DATA).content.decode("utf-8").splitlines()
     return random.choice(lines).strip()
 
 
 def format_message(message):
     lines = textwrap.wrap(message, 26)[:2]
     if len(lines) == 1:
-        lines.append('')
+        lines.append("")
 
     formatted_message = "%-26s%-26s" % (lines[0], lines[1])
     return formatted_message
 
 
 def send_message(message):
-    if '--pretend' in sys.argv:
+    if "--pretend" in sys.argv:
         print("Pretending: Set message to '%s'" % message)
         return
 
-    requests.get(
-        "http://localhost:8089/set-text/%s" % message
-    )
-    with open(MSG_CACHE_FILE, 'w') as fh:
+    requests.get("http://localhost:8089/set-text/%s" % message)
+    with open(MSG_CACHE_FILE, "w") as fh:
         fh.write(message)
 
 
 def is_own_message():
     """Return true if flappy is currently showing our own message"""
 
-    current_msg = requests.get('http://localhost:8089/get-text').content.decode('utf-8').strip()
+    current_msg = (
+        requests.get("http://localhost:8089/get-text").content.decode("utf-8").strip()
+    )
     try:
         with open(MSG_CACHE_FILE, "r") as fh:
             return fh.read().strip() == current_msg
@@ -70,26 +70,24 @@ def is_own_message():
 
 
 def lights_off():
-    if '--pretend' in sys.argv:
+    if "--pretend" in sys.argv:
         print("Pretending: lights off")
         return
-    requests.get(
-            "http://localhost:8089/light-off"
-    )
+    requests.get("http://localhost:8089/light-off")
 
 
-if __name__ == '__main__':
-    if '--debug' in sys.argv:
+if __name__ == "__main__":
+    if "--debug" in sys.argv:
         __import__("pdb").set_trace()
 
-    now = datetime.now().strftime('%a %d.%m.')
+    now = datetime.now().strftime("%a %d.%m.")
     message = "%s %s" % (now, new_message())
     formatted_message = format_message(message)
 
-    if is_own_message() or '--force' in sys.argv:
+    if is_own_message() or "--force" in sys.argv:
         # we can overwrite: forced or own message
         send_message(formatted_message)
 
-    if datetime.now().weekday() in (5,6):
+    if datetime.now().weekday() in (5, 6):
         # on weekends, light should be turned off
         lights_off()
